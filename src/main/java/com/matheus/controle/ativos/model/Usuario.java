7b906fff8b594ac74e20e5c85b19884108ea8757
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-01-24 17:55:22 -0300

    Iniciando o projeto

--- src/main/java/com/matheus/controle/ativos/model/Usuario.java
+++ src/main/java/com/matheus/controle/ativos/model/Usuario.java
@@ -2,6 +2,7 @@ package com.matheus.controle.ativos.model;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,8 +21,11 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column
     private String nome;
 
+
+    @Column
     private String senha;
     
 }
