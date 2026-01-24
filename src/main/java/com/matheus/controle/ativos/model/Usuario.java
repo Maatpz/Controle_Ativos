4827bd84982b3a67bc4f54b4d36e1368eec32261
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-01-24 17:51:13 -0300

    Iniciando o projeto

---
+++ src/main/java/com/matheus/controle/ativos/model/Usuario.java
@@ -0,0 +1,27 @@
+package com.matheus.controle.ativos.model;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String nome;
+
+    private String senha;
+    
+}
---
+++ src/test/java/com/matheus/controle/ativos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.controle.ativos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
