355d3e2479c2defe62ea90cb531dddde37df41b1
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-01-26 14:55:03 -0300

    feat: criar usuario

--- src/main/java/com/matheus/controle/ativos/model/Usuario.java
+++ src/main/java/com/matheus/controle/ativos/model/Usuario.java
@@ -2,16 +2,22 @@ package com.matheus.controle.ativos.model;
 
 import java.util.UUID;
 
+import com.matheus.controle.ativos.model.enums.Role;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name= "tb_usuarios")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -21,11 +27,20 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @NotBlank(message = "Nome de usuario obrigatório")
+    @Size(min = 3, max = 50, message = "Nome de usuario deve ser maior que três")
+    @Column(nullable = false)
+    private String username;
+
+    @NotBlank(message = "Senha obrigatória")
+    @Size(min = 6, message = "Senha deve ter no minimo 6 caracteres")
     @Column
+    private String password;
+
     private String nome;
 
+    private Boolean ativo = true;
 
-    @Column
-    private String senha;
+    private Role role = Role.ADMIN;
     
 }
