c7f7e47e079a375202fec808758eab275bee78a4
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-01-27 12:02:20 -0300

    feat: criar usuarioRepository

---
+++ src/main/java/com/matheus/controle/ativos/repository/UsuarioRepository.java
@@ -0,0 +1,18 @@
+package com.matheus.controle.ativos.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.matheus.controle.ativos.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository <Usuario, UUID> {
+
+    Optional<Usuario> findByUsername (String username);
+
+    boolean existsByUsername(String username);
+
+    Optional<Usuario> findByUsernameAndAtivo(String username, Boolean Ativo);
+    
+}
