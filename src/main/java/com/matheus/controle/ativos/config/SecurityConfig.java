5b1f08f7f1e72d4ef8adae1f737ff4470042f1ec
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-01-30 00:17:43 -0300

    feat: criar securityconfig

---
+++ src/main/java/com/matheus/controle/ativos/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.matheus.controle.ativos.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                // .antMatchers("/login", "/css/**", "/js/**", "/images/**",
+                // "/h2-console/**").permitAll()
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated() // Exigir autenticação para outras rotas
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/", true)
+                .failureUrl("/login?error=true")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout=true")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll()
+                .and();
+        // h2 console config teste
+        // .headers()
+        // .frameOptions().sameOrigin();
+
+        return http.build();
+    }
+}
