f2d5a2075a5661026826ab408514347557170a56
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-02-10 22:55:08 -0300

    Update Ativo.java

--- src/main/java/com/matheus/controle/ativos/model/Ativo.java
+++ src/main/java/com/matheus/controle/ativos/model/Ativo.java
@@ -7,6 +7,8 @@ import com.matheus.controle.ativos.model.enums.Status;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -33,7 +35,7 @@ public class Ativo {
     private String nomeAtivo;
 
     // private String dominio;
-
+    @Enumerated(EnumType.STRING)
     private Status status;
 
     private String localidade;
