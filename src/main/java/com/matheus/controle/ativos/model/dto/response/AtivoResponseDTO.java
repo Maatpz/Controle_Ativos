3de38b10b48c657ce189e84bdf1503b4369a2b65
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-04-29 00:21:36 -0300

    Update AtivoResponseDTO.java

--- src/main/java/com/matheus/controle/ativos/model/dto/response/AtivoResponseDTO.java
+++ src/main/java/com/matheus/controle/ativos/model/dto/response/AtivoResponseDTO.java
@@ -1,10 +1,13 @@
 package com.matheus.controle.ativos.model.dto.response;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import com.matheus.controle.ativos.model.enums.Status;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -13,9 +16,13 @@ public class AtivoResponseDTO {
 
     private UUID id;
     private String nomeAtivo;
-    private Status status;
-    private String localidade;
     private String setor;
     private String responsavel;
+    private String categoria;
     private String patrimonio;
+    private Status status;
+    private String macAddressEthernet;
+    private String observacoes;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
