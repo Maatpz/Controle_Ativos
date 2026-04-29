1fcd1e96c7423c469a310b82aff5a3821a0ac3e9
Matheus de Oliveira Pizzolato <166886779+Maatpz@users.noreply.github.com>
2026-04-29 00:19:07 -0300

    Create BusinessException.java

---
+++ src/main/java/com/matheus/controle/ativos/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.matheus.controle.ativos.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
